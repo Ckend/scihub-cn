513f29fe2cf6a73d893d7b157e17ff982eb6774e
ckend <83493903@qq.com>
2021-12-31 01:01:11 +0800

    feature: upload to pypi

---
+++ scihub-cn/__init__.py
@@ -0,0 +1,3 @@
+from scihub import main
+
+__all__ = ['main']
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='scihub-cn',
+    version='0.0.1',
+    author='ckend',
+    author_email='admin@pythondict.com',
+    url='https://github.com/Ckend/scihub-cn',
+    description=u'全网文献下载工具',
+    packages=['scihub-cn'],
+)
