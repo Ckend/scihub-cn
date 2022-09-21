5329020e15248f2fc3064aa2ed0cedb7494fe5ed
ckend <83493903@qq.com>
2022-09-22 00:46:10 +0800

    optimization: update version

--- setup.py
+++ setup.py
@@ -5,22 +5,7 @@ import os
 import requests
 from setuptools import setup
 
-# 将markdown格式转换为rst格式
-def md_to_rst(from_file, to_file):
-    r = requests.post(url='http://c.docverter.com/convert',
-                      data={'to': 'rst', 'from': 'markdown'},
-                      files={'input_files[]': open(from_file, 'rb')})
-    if r.ok:
-        with open(to_file, "wb") as f:
-            f.write(r.content)
-
-
-md_to_rst("README.md", "README.rst")
-
-if os.path.exists('README.rst'):
-    long_description = open('README.rst', encoding="utf-8").read()
-else:
-    long_description = 'Add a fallback short description here'
+long_description = open('README.md', encoding="utf-8").read()
 
 if os.path.exists("requirements.txt"):
     install_requires = io.open("requirements.txt").read().split("\n")
@@ -29,7 +14,7 @@ else:
 
 setup(
     name='scihub-cn',
-    version='0.0.14',
+    version='0.0.16',
     author='ckend',
     author_email='admin@pythondict.com',
     url='https://github.com/Ckend/scihub-cn',
